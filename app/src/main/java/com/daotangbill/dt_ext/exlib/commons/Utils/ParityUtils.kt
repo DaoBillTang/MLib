2e3e9b4575215513d3e6a2d8e0993f87c832b959
1750352866@qq.com <you5TXwudi>
2017-09-22 11:26:51 +0800

    1、尝试 校验方法 fixbug

--- app/src/main/java/com/daotangbill/dt_ext/exlib/commons/Utils/ParityUtils.kt
+++ app/src/main/java/com/daotangbill/dt_ext/exlib/commons/Utils/ParityUtils.kt
@@ -24,10 +24,10 @@ fun String?.parity(context: Context?, regular: String?, err: String = "校验错
             false
         } else {
             if (Pattern.matches(regular, this)) {
+                true
+            } else {
                 context?.Terror(err)
                 false
-            } else {
-                true
             }
         }
 
