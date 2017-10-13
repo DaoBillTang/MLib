38bf8d514e043c327dccc3490966a549dd1cf990
1750352866@qq.com <you5TXwudi>
2017-10-13 11:48:47 +0800

    添加：对于edittext 的处理 和 baseAdapter

--- app/src/main/java/com/daotangbill/dt_ext/exlib/commons/view/EditExt.kt
+++ app/src/main/java/com/daotangbill/dt_ext/exlib/commons/view/EditExt.kt
@@ -20,7 +20,9 @@ fun EditText?.Filter(filterStr: String?) {
 }
 
 fun EditText?.Filter(filterStr: String, max: Int) {
+
     val filters = arrayOfNulls<InputFilter>(2)
+
     filters[0] = MyInputFilter(filterStr)
     filters[1] = InputFilter.LengthFilter(max)
     this?.filters = filters
