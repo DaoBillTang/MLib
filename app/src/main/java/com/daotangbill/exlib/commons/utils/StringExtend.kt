e81f1fe6f425f925dc4245dd6cfa125276be38a2
1750352866@qq.com <you5TXwudi>
2017-12-08 16:14:38 +0800

    更新 stringExt方法

--- app/src/main/java/com/daotangbill/exlib/commons/utils/StringExtend.kt
+++ app/src/main/java/com/daotangbill/exlib/commons/utils/StringExtend.kt
@@ -78,7 +78,7 @@ fun Editable?.judgeNumber(intNum: Int, decimalNum: Int): String? {
         }
     }
 
-    val posDot = temp.indexOf("")//返回指定字符在此字符串中第一次出现处的索引
+    val posDot = temp.indexOf(".")//返回指定字符在此字符串中第一次出现处的索引
     if (posDot <= 0) {//不包含小数点
         if (temp.length <= intNum) {
             //小于五位数直接返回
@@ -87,6 +87,11 @@ fun Editable?.judgeNumber(intNum: Int, decimalNum: Int): String? {
             this.delete(intNum, intNum + 1)//大于五位数就删掉第六位（只会保留五位）
             return this.toString()
         }
+    }
+    val otherDot = temp.indexOf(".", posDot+1)
+    if (otherDot >= 0) {
+        this.delete(otherDot, otherDot + 1)
+        return this.toString()
     }
     if (temp.length - posDot - 1 > decimalNum) {   //如果包含小数点
         this.delete(posDot + decimalNum + 1, posDot + decimalNum + 2)//删除小数点后的第三位
