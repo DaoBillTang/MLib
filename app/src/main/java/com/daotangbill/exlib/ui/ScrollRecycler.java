f337e009763ae269c182dbadb725ddd86b052f73
bill <1750352866@qq.com>
2018-02-09 14:11:09 +0800

    添加了： 可以拦截所有操作的 Linerlayout, 添加了： 可以在ScrollView中使用的RecyclerView

---
+++ app/src/main/java/com/daotangbill/exlib/ui/ScrollRecycler.java
@@ -0,0 +1,81 @@
+package com.daotangbill.exlib.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by BILL on 2017/4/5.
+ * email:tangbaozi@daotangbill.uu.me
+ *
+ * @author BILL
+ * @version 可以在 scroll view 中正确显示的 RecyclerView
+ */
+public class ScrollRecycler extends RecyclerView {
+    public ScrollRecycler(Context context) {
+        super(context);
+    }
+
+    public ScrollRecycler(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.setNestedScrollingEnabled(false);
+    }
+
+    public ScrollRecycler(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+    private View emptyView;
+
+    final private AdapterDataObserver observer = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            checkIfEmpty();
+        }
+    };
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        final Adapter oldAdapter = getAdapter();
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(observer);
+        }
+        super.setAdapter(adapter);
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(observer);
+        }
+        checkIfEmpty();
+    }
+
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+        checkIfEmpty();
+    }
+
+    void checkIfEmpty() {
+        if (emptyView != null && getAdapter() != null) {
+            final boolean emptyViewVisible = getAdapter().getItemCount() == 0;
+            emptyView.setVisibility(emptyViewVisible ? VISIBLE : GONE);
+            setVisibility(emptyViewVisible ? GONE : VISIBLE);
+        }
+    }
+}
