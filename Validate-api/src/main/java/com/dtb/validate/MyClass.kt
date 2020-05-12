b788ebe66c8477e02788e199bdd1a57da668da69
tangl <tangl@aplus-it.cn>
2020-05-12 20:21:19 +0800

    2.0.1； 迁移至分支 dtbUtils2,并更改了结构； 修改了原本RxLifeCycle,改为AndroidX 的lifecycle； 提交WorkerManager的内容 在 arouter 的基础上尝试修改

---
+++ Validate-api/src/main/java/com/dtb/validate/MyClass.kt
@@ -0,0 +1,4 @@
+package com.dtb.validate
+
+public class MyClass {
+}
---
+++ validate-annotation/src/main/java/com/dtb/validate/MyClass.kt
@@ -0,0 +1,23 @@
+package com.dtb.validate
+
+public class MyClass {
+
+    class A {
+
+
+        var A: String? = null
+
+    }
+
+    class Validate {
+
+
+        fun validate(bean: A) {
+
+        }
+
+
+    }
+
+
+}
